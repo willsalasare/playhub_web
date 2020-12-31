7f797033d4e007c56f7206f5ec8d4dbe98f750b8
Will Salas <willsalasare@gmail.com>
2020-12-31 15:32:48 -0600

    new v

--- pages/index.vue
+++ pages/index.vue
@@ -7,13 +7,13 @@
       <div class="p-4 flex items-center">
         <div>
           <p class="text-2xl">
-            ¿Que esperas? Descarga la app y comienza a disfrutar de tu
-            contenido favorio.
+            ¿Que esperas? Descarga la app y comienza a disfrutar de tu contenido
+            favorio.
           </p>
           <div class="mt-3">
             <a
               class="btn py-2 px-4 bg-yellow-850 focus:outline:none rounded-lg"
-              href="/playhubplus.apk"
+              href="/release.apk"
             >
               Descargar
             </a>
